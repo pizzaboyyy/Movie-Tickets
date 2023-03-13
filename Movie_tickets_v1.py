6e345c56cc693dbc6388014dc1784b27c1e8a7fd
pizzaboyyy <121211168+pizzaboyyy@users.noreply.github.com>
2023-03-14 10:12:42 +1300

    Changes

---
+++ Movie_tickets_v1.py
@@ -0,0 +1,24 @@
+         # """Movie theatre ticketing system - v1
+         # welcome screen and variables
+         # Created by James Mulholland"""
+
+
+         # Component 1 - Welcome screen and set up variables
+         def sell_ticket():
+             print("********** Panfare Movies - ticketing system **********\n")
+
+             adult_tickets = 0
+             student_tickets = 0
+             child_tickets = 0
+             gift_tickets = 0
+             tickets_sold = 0
+             total_sales = 0
+
+
+             # Main routine
+             sell_ticket()
+
+
+
+
+
